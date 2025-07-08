269159e574857b12aa617ccb69c4cf93e18ef761
NavarroDc <130122601+NavarroDc@users.noreply.github.com>
2025-07-08 15:30:51 -0600

    prueba 1

--- ED DavidNavarroC-Proyecto2/src/ed/davidnavarroc/proyecto2/EDDavidNavarroCProyecto2.java
+++ ED DavidNavarroC-Proyecto2/src/ed/davidnavarroc/proyecto2/EDDavidNavarroCProyecto2.java
@@ -10,11 +10,9 @@ package ed.davidnavarroc.proyecto2;
  */
 public class EDDavidNavarroCProyecto2 {
 
-    /**
-     * @param args the command line arguments
-     */
+  
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("prueba githubbb");
     }
     
 }

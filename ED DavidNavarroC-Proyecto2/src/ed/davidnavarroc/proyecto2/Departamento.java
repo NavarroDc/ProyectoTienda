ac0b552312b29345bc7496e7458777712632c8ee
NavarroDc <130122601+NavarroDc@users.noreply.github.com>
2025-07-08 17:52:22 -0600

    Menú se ve y clases creadas
    Articulo
    Departamento
    Gestor

---
+++ ED DavidNavarroC-Proyecto2/src/ed/davidnavarroc/proyecto2/Departamento.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ed.davidnavarroc.proyecto2;
+
+public class Departamento {
+    
+    private int id;
+    private String nombre;
+    private Articulo[] articulos = new Articulo[20];
+    private int frenteCola = 0;
+    private int finalCola = -1;
+    private int numActualCola = 0;
+    
+}

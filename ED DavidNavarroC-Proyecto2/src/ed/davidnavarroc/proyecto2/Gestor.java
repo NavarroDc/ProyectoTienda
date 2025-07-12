ae8be00237314ff2087cbea015d72ca668655896
NavarroDc <130122601+NavarroDc@users.noreply.github.com>
2025-07-11 18:30:05 -0600

    clase Gestor comentada 1

--- ED DavidNavarroC-Proyecto2/src/ed/davidnavarroc/proyecto2/Gestor.java
+++ ED DavidNavarroC-Proyecto2/src/ed/davidnavarroc/proyecto2/Gestor.java
@@ -8,12 +8,13 @@ import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
 
 public class Gestor {
-    
+    //Propiedades de la clase Gestor
     private Departamento[] departamentos;
     private int ultimoDep;
     private int idDepartamento;
     private int idArticulo;
 
+    //Constructor de la clase Gestor
     public Gestor() {
         this.departamentos = new Departamento[20];
         this.ultimoDep = -1;
@@ -23,6 +24,7 @@ public class Gestor {
 
     //------------------------------------------------------
 
+    //Getters y Setters
     public Departamento[] getDepartamentos() {
         return departamentos;
     }
@@ -57,44 +59,44 @@ public class Gestor {
     
     //------------------------------------------------------
     
+    //Método para verificar si la pila está de Departamentos está llena
     public boolean pilaLlena(){
-        boolean pLlena = false;
-        if(ultimoDep == departamentos.length - 1){
+        boolean pLlena = false;//Valor booleano para identificar si la pila está llena
+        if(ultimoDep == departamentos.length - 1){//Si el índice del último departamento es igual a 19 significa que la pila está llena
             pLlena = true;
         }
-        return pLlena;
+        return pLlena;//Retorna el booleado para verificar si está llena
     }
     
+    //Método para verificar si una pila está vacía
     public boolean pilaVacia(){
-        boolean pVacia = false;
-        if(ultimoDep == -1){
+        boolean pVacia = false;//Valor booleano para identificar si la pila está vacía
+        if(ultimoDep == -1){//Si el índice del último departamento es igual a -1 significa que la pila está vacía
             pVacia = true;
         }
-        return pVacia;
+        return pVacia;//Retorna el booleano para verificar si está vacía
     }
     
+    //Método para agregar un departamento
     public boolean agregarDepartamento(String nombreDepartamento){
-        if(pilaLlena()){
+        if(pilaLlena()){//Si la pila de Departamentos está llena, retorna falso y no continúa
             return false;
         }
         
         if(nombreDepartamento == null || nombreDepartamento.trim().isEmpty()){
-            return false;
+            return false;//Si el nombre del departamento está vacío o vacío, retorna falso y no continúa
         }
         
+        // Crea un nuevo objeto Departamento con el ID actual y el nombre ingresado (eliminando espacios extra)
         Departamento nuevoDepartamento = new Departamento(idDepartamento, nombreDepartamento.trim());
-        departamentos[++ultimoDep] = nuevoDepartamento;
-        idDepartamento++;
+        departamentos[++ultimoDep] = nuevoDepartamento;//Inserta el nuevo departamento en la siguiente posición de la pila e incrementa el índice
+        idDepartamento++;//Se incrementa el ID después de insertar el departamento
         
-        return true;
+        return true;//El retorno true indica que el departamento se agregó correctamente
     }
     
+    //Método que aumenta el contador de ID de artículos en 1. Se utiliza después de agregar un nuevo artículo para que sea consecutivo
     public void incrementarIdArticulo() {
     idArticulo++;
-    
     }
-    
-    
-    
-    
 }
